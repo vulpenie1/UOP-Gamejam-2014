d77abf153453592a2eaef27842a075a4517090ef
Kierz <kierz@users.noreply.github.com>
2014-06-10 18:28:38 +0100

    Merged

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -1,10 +1,23 @@
-﻿using UnityEngine;
+﻿/*
+	File:			GameManager.cs
+	Author:			Krz, Dan
+	Project:		Curling Game
+	Soundtrack:		Station 90 Show 13: Simon Heartfield and Manni Dee
+	Description:	Manages the state of the game.
+*/
+
+using UnityEngine;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-	public GameObject stonesDeposit;
+    static Vector2 BULLSEYE_POSITION = new Vector2(10.0f, 10.0f);
+    static private int team1score = 0, team2score = 0;
+    public GameObject stonesDeposit;
 
-	void Awake() {
+    void Awake()
+    {
+        Screen.lockCursor = true;
+        Screen.showCursor = false;
 	}
 	
 	void Update() {	
@@ -23,4 +36,80 @@ public class GameManager : MonoBehaviour {
 
 		return gm;
 	}
+
+
+    static void UpdateScores()
+    {
+        eTeam winningTeam = GetRoundWinner();
+        float nmeDistanceFromBullseye = GetNMEClosestToBullseye(winningTeam);
+        GivePoints(winningTeam, nmeDistanceFromBullseye);
+    }
+
+    static eTeam GetRoundWinner()
+    {
+        eTeam winningTeam = 0;
+        float winningDistance = 99999.9f;
+
+        foreach (Rock rock in FindObjectsOfType<Rock>())
+        {
+            if (rock.DistanceFromBullseye() < winningDistance)
+            {
+                winningTeam = rock.team;
+                winningDistance = rock.DistanceFromBullseye();
+            }
+        }
+
+        return winningTeam;
+    }
+
+    static float GetNMEClosestToBullseye(eTeam winningTeam)
+    {
+        float closestToBullseye = 99999.9f;
+        foreach (Rock rock in FindObjectsOfType<Rock>())
+        {
+            if (rock.team != winningTeam)
+            {
+                if (rock.DistanceFromBullseye() < closestToBullseye)
+                {
+                    closestToBullseye = rock.DistanceFromBullseye();
+                }
+            }
+        }
+
+        return closestToBullseye;
+    }
+
+    static private void GivePoints(eTeam winningTeam, float nmeDistanceFromBullseye)
+    {
+        int points = 0;
+
+        foreach (Rock rock in FindObjectsOfType<Rock>())
+        {
+            if (rock.team == winningTeam)
+            {
+                if (rock.DistanceFromBullseye() < nmeDistanceFromBullseye)
+                {
+                    points++;
+                }
+            }
+        }
+        GiveWinningTeamPoints(winningTeam, points);
+    }
+
+    static private void GiveWinningTeamPoints(eTeam team, int points)
+    {
+        switch (team)
+        {
+            case eTeam.TEAM_1:
+                {
+                    team1score += points;
+                    break;
+                }
+            case eTeam.TEAM_2:
+                {
+                    team2score += points;
+                    break;
+                }
+        }
+    }
 }
