ddce78332fa99e409011fab6ed8e895112b92bcb
floorman <daniel.guy.moody@gmail.com>
2014-06-10 13:32:29 +0100

    Can now shoot the rock

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	void Awake() {
+	}
+	
+	void Update() {	
+	}
+
+	enum eTeam {
+		TEAM_1,
+		TEAM_2
+	}
+
+	private static GameManager gm;
+	public static GameManager Singleton() {
+		if ( !gm ) {
+			gm = FindObjectOfType<GameManager>();
+		}
+
+		return gm;
+	}
+}
