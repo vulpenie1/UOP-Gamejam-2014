24b3225bb6df10d3d42a030e2299e74b419c0203
race122 <zack.dutton@hotmail.com>
2014-06-10 14:02:24 +0100

    Added some menu stuff

---
+++ Assets/Code/menuItemOutline.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuItemOutline : MonoBehaviour {
+
+	public GameObject outline;
+
+	// Use this for initialization
+	void Start () 
+	{
+		outline.renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnMouseEnter()
+	{
+		Debug.Log("Test");
+		outline.renderer.enabled = true;
+	}
+}
---
+++ Assets/Code/menuScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public string itemName;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnMouseDown()
+	{
+		if ( itemName == "Start Game" )
+		{
+			Debug.Log("Start Game");
+			Application.LoadLevel("dansTest");
+		}
+		
+		else if ( itemName == "Options" )
+		{
+			Debug.Log("Options Menu");
+			Application.LoadLevel("OptionsMenu");
+		}
+		
+		else if ( itemName == "Credits" )
+		{
+			Debug.Log("Credits");
+			Application.LoadLevel("CreditsMenu");
+		}	
+		
+		else if ( itemName == "Exit" )
+		{
+			Debug.Log("Exit");
+			Application.Quit();
+		}
+		
+		else if ( itemName == "Back" )
+		{
+			Debug.Log("Back");
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
