df79f1097b719384e9bca5c3bb657748d5132fad
race122 <zack.dutton@hotmail.com>
2014-06-10 14:17:04 +0100

    Menu stuff again

--- Assets/Code/menuScript.cs
+++ Assets/Code/menuScript.cs
@@ -14,4 +14,35 @@ public class menuScript : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	void OnMouseDown()
+	{
+		if ( name == "start game" )
+		{
+			Debug.Log("Start Game");
+			Application.LoadLevel("dansTest");
+		}
+
+		if ( name == "options" )
+		{
+			Debug.Log("Options");
+			Application.LoadLevel("optionsMenu");
+		}
+
+
+		if ( name == "credits" )
+		{
+			Debug.Log("Credits");
+			Application.LoadLevel("creditsMenu");
+		}
+
+
+		if ( name == "exit" )
+		{
+			Debug.Log("Exit");
+			Application.Quit();
+		}
+
+
+	}
 }
