65f2df19f844f4b3aa15f2a034b79a0be8ec0d63
floorman <daniel.guy.moody@gmail.com>
2014-06-11 15:30:57 +0100

    Trying a new sliding script

---
+++ Assets/Code/Player2.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2 : MonoBehaviour {
+	private bool canControl = true;
+	private bool isMoving = false;
+	private float speed = 0.0f;
+	private float accel = 0.005f;
+	private float decel = 0.0025f;
+	private const float MAX_SPEED = 0.1f;
+
+	void Start() {
+		
+	}
+	
+	float dx, dz;
+
+	void Update() {
+		if ( canControl ) {
+			if ( Input.GetKey( KeyCode.W ) ) {
+				dz += accel;
+			}
+
+			if ( Input.GetKey( KeyCode.S ) ) {
+				dz -= accel;
+			}
+
+			if ( Input.GetKey( KeyCode.A ) ) {
+				dx -= accel;
+			}
+
+			if ( Input.GetKey( KeyCode.D ) ) {
+				dx += accel;
+			}
+
+			print( speed );
+		}
+
+		// I feel dirty...
+		if ( Input.GetKey( KeyCode.W ) || Input.GetKey( KeyCode.A ) || Input.GetKey( KeyCode.S ) || Input.GetKey( KeyCode.D ) ) {
+			if ( speed < MAX_SPEED ) {
+				speed += accel;
+			}
+		} else {
+			if ( rigidbody.velocity.magnitude > 0 && speed > 0 ) {
+				speed -= decel;
+			}
+		}
+
+		Vector3 direction = new Vector3( dx * speed, 0f, dz * speed );
+		transform.Translate( direction );
+	}
+}
