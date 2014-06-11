79345058ff458dc5de78d7475a51ca59cc77380e
Aidan O'Leary <aidanoleary92@gmail.com>
2014-06-11 14:13:01 +0100

    Made changes to HudScript also created Hud Scene

--- Assets/Code/HudScript.cs
+++ Assets/Code/HudScript.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class HudScript : MonoBehaviour {
 
-	private int score1 = 0;
-	private int score2 = 0;
+	//private int score1 = 0;
+	//private int score2 = 0;
+	public GameObject score1;
+	public GameObject score2;
+	public GameObject currentTeam;
 
 	// Use this for initialization
 	void Start () {
@@ -18,23 +21,23 @@ public class HudScript : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		updateScores ();
-		updateCurrentPlayer ();
+		UpdateScores ();
+		UpdateCurrentPlayer ();
 
 	}
 
-	private void updateScores() {
+	//A function that updates the display for teams current scores
+	private void UpdateScores() {
 		int[] scores = GameManager.getScore ();
-		score1 = scores [0];
-		score2 = scores [1];
-		
-		GUIText team1Score = GetComponent("Team1Score") as GUIText;
-		GUIText team2score = GetComponent("Team2Score") as GUIText;
-		team1Score.text = "Team1: " + score1.ToString();
-		team2score.text = "Team2: " + score2.ToString();
+		score1.guiText.text = "Team 1: " + scores[0].ToString();
+		score2.guiText.text = "Team 2: " + scores[1].ToString();
 	}
 
-	private void updateCurrentPlayer() {
+	//A function that updates the display for the current team
+	// --------- Change this when I have pulled the latest repo ----------
+	private void UpdateCurrentPlayer() {
+		//currentTeam = GameManager.IsTeamOne();
+		currentTeam.guiText.text = "Team 1's turn";
 
 
 	}
