283472d6be9fe313668f770dfe8aa6bc560d537a
Aidan O'Leary <aidanoleary92@gmail.com>
2014-06-11 11:41:42 +0100

    Did more work on HUD

--- Assets/Code/HudScript.cs
+++ Assets/Code/HudScript.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class HudScript : MonoBehaviour {
 
+	private int score1 = 0;
+	private int score2 = 0;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,30 @@ public class HudScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
 	}
+
+	void OnGUI() {
+		updateScores ();
+		updateCurrentPlayer ();
+
+	}
+
+	private void updateScores() {
+		int[] scores = GameManager.getScore ();
+		score1 = scores [0];
+		score2 = scores [1];
+		
+		GUIText team1Score = GetComponent("Team1Score") as GUIText;
+		GUIText team2score = GetComponent("Team2Score") as GUIText;
+		team1Score.text = "Team1: " + score1.ToString();
+		team2score.text = "Team2: " + score2.ToString();
+	}
+
+	private void updateCurrentPlayer() {
+
+
+	}
+	
 }
